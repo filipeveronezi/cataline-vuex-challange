77cf18999d54cc04a46672061ad523dfad9a57a2
Filipe Veronezi <filipeseidi@hotmail.com>
2021-02-12 03:45:48 -0300

    Card size

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -15,10 +15,8 @@ export default {
 
 <style>
 #card {
-  height: 60%;
-  min-height: 457px;
-  width: 22%;
-  min-width: 305px;
+  height: 457px;
+  width: 305px;
   box-shadow: 0 0 1rem 0rem rgba(0, 0, 0, 0.75);
   border-radius: 15px;
   background: linear-gradient(180deg, #5d668134, #3f445226);
